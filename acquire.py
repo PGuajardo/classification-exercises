7ad1ccadc7c6afb55d1981da0ed15ea84ffa22f9
Paige Guajardo <paigeeguajardo@gmail.com>
2021-11-09 15:43:21 -0600

    Uploaded acquire .py file/ Updated classification_exercises .ipynb file

---
+++ acquire.py
@@ -0,0 +1,45 @@
+import env
+import pandas as pd
+import os
+
+#Gets connection to Code Up database using env file
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+# Get titanic .csv Data
+def get_titanic_data():
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        titanic = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+        titanic.to_csv(index = False)
+    return titanic
+
+
+# Get iris .csv Data
+def get_iris_data():
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        iris = pd.read_sql('SELECT * FROM species JOIN measurements using(species_id)', get_connection('iris_db'))
+        iris.to_csv(index = False)
+    return iris
+
+
+
+# Get telco_churn .csv Data
+def get_telco_data():
+    filename = "teleco_churn.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        teleco_churn = pd.read_sql('SELECT * FROM customers JOIN contract_types using(contract_type_id) JOIN internet_service_types using(internet_service_type_id) JOIN payment_types using(payment_type_id)', 
+        get_connection('teleco_churn'))
+        telco_churn.to_csv(index = False)
+    return telco_churn
