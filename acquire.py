540285ec88e3a42fe891edb12daae8144b9883da
Paige Guajardo <paigeeguajardo@gmail.com>
2021-11-12 11:26:21 -0600

    Uploaded prepare.py file/ Update acquire .py file/ Update classification_exercises .ipynb file

--- acquire.py
+++ acquire.py
@@ -12,7 +12,7 @@ def get_titanic_data():
     filename = "titanic.csv"
 
     if os.path.isfile(filename):
-        return pd.read_csv(filename)
+        titanic = pd.read_csv(filename)
     else:
         titanic = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
         titanic.to_csv(index = False)
@@ -24,22 +24,20 @@ def get_iris_data():
     filename = "iris.csv"
 
     if os.path.isfile(filename):
-        return pd.read_csv(filename)
+        iris = pd.read_csv(filename)
     else:
         iris = pd.read_sql('SELECT * FROM species JOIN measurements using(species_id)', get_connection('iris_db'))
         iris.to_csv(index = False)
     return iris
 
 
-
-# Get telco_churn .csv Data
 def get_telco_data():
     filename = "teleco_churn.csv"
 
     if os.path.isfile(filename):
-        return pd.read_csv(filename)
+        telco_churn = pd.read_csv(filename)
     else:
-        teleco_churn = pd.read_sql('SELECT * FROM customers JOIN contract_types using(contract_type_id) JOIN internet_service_types using(internet_service_type_id) JOIN payment_types using(payment_type_id)', 
-        get_connection('teleco_churn'))
+        telco_churn = pd.read_sql('SELECT * FROM customers JOIN contract_types using(contract_type_id) JOIN internet_service_types using(internet_service_type_id) JOIN payment_types using(payment_type_id)', 
+        get_connection('telco_churn'))
         telco_churn.to_csv(index = False)
     return telco_churn
