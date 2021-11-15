f078f7ca332dfc01803cb6a03a86789f3182ce06
Paige Guajardo <paigeeguajardo@gmail.com>
2021-11-15 14:39:17 -0600

    Updated classification ipynb file/ Updated prepare py file

--- prepare.py
+++ prepare.py
@@ -37,11 +37,8 @@ def clean_data(df):
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
-
-    def split_data(df):
-    '''
-    Takes in a dataframe and return train, validate, test subset dataframe
-    '''
+#Takes in a dataframe and return train, validate, test subset dataframe
+def split_data(df):
     train, test = train_test_split(df, test_size = .2, random_state = 123, stratify = df.survived)
     train, validate = train_test_split(train, test_size =  .3, random_state= 123, stratify = train.survived)
     return train, validate, test
@@ -51,7 +48,7 @@ def clean_data(df):
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
-    def impute_mode(train, validate, test):
+def impute_mode(train, validate, test):
     '''
     Takes in train, validate , test, and uses train to identify the best value to replace nulls in embark_town
     Imputes that values into all three sets and returns all three sets
@@ -60,24 +57,18 @@ def clean_data(df):
     imputer = SimpleImputer(missing_values = np.nan, strategy = 'most_frequent')
     
     #imputer = imputer.fit(train[['embark_town']])
-    
-    
     train[['embark_town']] = imputer.fit_transform(train[['embark_town']])
-
     validate[['embark_town']] = imputer.fit_transform(validate[['embark_town']])
-
     test[['embark_town']] = imputer.fit_transform(test[['embark_town']])
-    
     return train, validate, test
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
-    def prep_titanic_data(df):
+def prep_titanic_data(df):
     '''
     Ultimate dishwasher
     '''
-    
     df = clean_data(df)
     train, validate, test = split_data(df)
     
